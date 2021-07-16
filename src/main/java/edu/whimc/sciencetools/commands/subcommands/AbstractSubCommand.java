5374c5bef491936d18514643e26d83c086feb2a2
emicb <emicb55@gmail.com>
2021-07-16 15:10:55 -0500

    Added Comments To AbstractSubCommand.java

--- src/main/java/edu/whimc/sciencetools/commands/subcommands/AbstractSubCommand.java
+++ src/main/java/edu/whimc/sciencetools/commands/subcommands/AbstractSubCommand.java
@@ -10,31 +10,60 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * This class is a template for all SubCommands.
+ */
 public abstract class AbstractSubCommand {
 
+    /**
+     * Permission levels that players can have.
+     */
     public enum Permission {
         ADMIN("admin"),
         USER("user");
 
         private final String perm;
 
+        /**
+         * Constructs a permission level with the specified name.
+         *
+         * @param perm the name of the permission
+         */
         Permission(String perm) {
             this.perm = perm;
         }
 
+        /**
+         * Converts the permission to a String in the form: "sciencetools.perm".
+         *
+         * @return the permission as a String
+         */
         @Override
         public @NotNull String toString() {
             return "sciencetools." + perm;
         }
-
     }
 
+    /* The command's name. */
     private final String command;
+    /* The arguments required to run the command. */
     private final List<String> args;
+    /* Any additional arguments for the command. */
     private final List<String> optionalArgs;
+    /* A description of what the command does. */
     private final String description;
+    /* The permission level required to use the command. */
     private final Permission permission;
 
+    /**
+     * Constructs a command with the given arguments.
+     *
+     * @param command the name of the command
+     * @param args the arguments required
+     * @param optionalArgs any optional arguments
+     * @param description a description of what the command does
+     * @param permission the required permission level to use the command
+     */
     public AbstractSubCommand(String command, List<String> args, List<String> optionalArgs, String description, Permission permission) {
         this.command = command;
         this.args = args == null ? Arrays.asList() : args;
@@ -43,24 +72,59 @@ public abstract class AbstractSubCommand {
         this.permission = permission;
     }
 
+    /**
+     * Runs when command is run.
+     *
+     * @param sender the command's sender
+     * @param args the arguments passed
+     * @return whether the command has been run or not
+     */
     protected abstract boolean commandRoutine(CommandSender sender, String[] args);
 
+    /**
+     * Runs on tab press.
+     *
+     * @param sender the command's sender
+     * @param args the arguments passed
+     * @return an empty List of Strings
+     */
     protected List<String> tabRoutine(CommandSender sender, String[] args) {
         return Arrays.asList();
     }
 
+    /**
+     * Runs when command is not passed enough arguments.
+     *
+     * @param sender the command's sender
+     */
     protected void notEnoughArgs(CommandSender sender) {
         return;
     }
 
+    /**
+     * Gets the full description of the SubCommand.
+     *
+     * @return the full description fo the SubCommand
+     */
     public @NotNull String fullDescription() {
         return this + " &7- " + this.description;
     }
 
+    /**
+     * Checks if SubCommand has any optional arguments.
+     *
+     * @return whether or not the SubCommand has optional arguments
+     */
     public boolean hasOptionalArgs() {
         return this.optionalArgs.size() != 0;
     }
 
+    /**
+     * Checks if the SubCommand has been passed enough arguments.
+     *
+     * @param argsLen the number (as an integer) of passed in arguments
+     * @return whether or not the SubCommand has been passed enough arguments
+     */
     public boolean hasEnoughArguments(int argsLen) {
         if (argsLen == this.args.size()) {
             return true;
@@ -68,7 +132,14 @@ public abstract class AbstractSubCommand {
         return argsLen >= this.args.size() + this.optionalArgs.size();
     }
 
+    /**
+     * Formats the argument.
+     *
+     * @param ind the index of the argument
+     * @return the formatted argument
+     */
     public @NotNull String formattedArg(int ind) {
+        // ensure argument index is valid
         if (args == null || ind < 0 || ind >= args.size()) {
             return "";
         }
@@ -76,7 +147,14 @@ public abstract class AbstractSubCommand {
         return "&7<&b" + args.get(ind) + "&7>";
     }
 
+    /**
+     * Formats the optional argument.
+     *
+     * @param ind the index of the optional argument
+     * @return the formatted optional argument
+     */
     public @NotNull String formattedOptionalArg(int ind) {
+        // ensure the optional argument index is valid
         if (optionalArgs == null || ind < 0 || ind >= optionalArgs.size()) {
             return "";
         }
@@ -84,6 +162,11 @@ public abstract class AbstractSubCommand {
         return "&7[&b" + optionalArgs.get(ind) + "&7]";
     }
 
+    /**
+     * Gets the command's usage.
+     *
+     * @return a String specifying how the command is used.
+     */
     public @NotNull String getUsage() {
         return "&e/sciencetools " + command + " " + String.join(" ", IntStream
                 .range(0, args.size())
@@ -91,6 +174,11 @@ public abstract class AbstractSubCommand {
                 .collect(Collectors.toList()));
     }
 
+    /**
+     * Gets the command's optional usage.
+     *
+     * @return a String specifying how the command is used.
+     */
     public @NotNull String getOptionalUsage() {
         return getUsage() + " " + String.join(" ", IntStream
                 .range(0, optionalArgs.size())
@@ -98,28 +186,53 @@ public abstract class AbstractSubCommand {
                 .collect(Collectors.toList()));
     }
 
+    /**
+     * Converts the SubCommand into a String.
+     *
+     * @return the SubCommand as a String.
+     */
     @Override
     public @NotNull String toString() {
         return "&e&o/sciencetools " + command;
     }
 
+    /**
+     * Gets the minimum amount of arguments required to run the command.
+     *
+     * @return the minimum number of arguments required to run the command
+     */
     public int getMinArgs() {
         return args.size();
     }
 
+    /**
+     * Gets the maximum number of arguments that can be passed into the command.
+     *
+     * @return the maximum number of arguments that can be passed into the command
+     */
     public int getMaxArgs() {
         return getMinArgs() + optionalArgs.size();
     }
 
+    /**
+     * Checks if the command has been executed.
+     *
+     * @param sender the command's sender
+     * @param args the passed arguments
+     * @return whether the command has been executed
+     */
     public boolean executeCommand(CommandSender sender, String[] args) {
-
+        // check if the user has sufficient permissions to run command
         if (!sender.hasPermission(this.permission.toString())) {
             Utils.msg(sender, "&cYou are missing the permission \"&4" + this.permission + "&c\" to use this command!");
             return false;
         }
 
+        // check if command has enough arguments passed into it
         if (!hasEnoughArguments(args.length - 1)) {
             List<String> missedCmdsList = new ArrayList<>();
+
+            // loop through and collect all the missing arguments
             for (int ind = args.length - 1; ind < this.args.size(); ind++) {
                 missedCmdsList.add(formattedArg(ind));
             }
@@ -129,6 +242,7 @@ public abstract class AbstractSubCommand {
 
             String missedCmds = String.join("&7, ", missedCmdsList);
 
+            // notify user of what arguemnts they are missing
             Utils.msg(sender, "&cMissing argument" + (missedCmdsList.size() > 1 ? "s" : "") + ": " + missedCmds);
             if (hasOptionalArgs()) {
                 Utils.msg(sender, "&cUsages:", "  " + getUsage(), "  " + getOptionalUsage());
@@ -143,10 +257,19 @@ public abstract class AbstractSubCommand {
         return commandRoutine(sender, Arrays.copyOfRange(args, 1, args.length));
     }
 
+    /**
+     * Runs on tab press.
+     *
+     * @param sender the command's sender
+     * @param args the passed arguments
+     * @return an empty List of Strings
+     */
     public List<String> executeTab(CommandSender sender, String[] args) {
+        // check if user has sufficient permissions or if there are too many arguments passed
         if (!sender.hasPermission(this.permission.toString()) || args.length > getMaxArgs()) {
             return Arrays.asList();
         }
+
         return tabRoutine(sender, args);
     }
 
