b02650f37b9d9d1b0ffb587ab89d780cd7c27741
emicb <emicb55@gmail.com>
2021-07-19 20:03:03 -0500

    Added Comments To Reload.java

--- src/main/java/edu/whimc/sciencetools/commands/subcommands/Reload.java
+++ src/main/java/edu/whimc/sciencetools/commands/subcommands/Reload.java
@@ -4,12 +4,25 @@ import edu.whimc.sciencetools.ScienceTools;
 import edu.whimc.sciencetools.utils.Utils;
 import org.bukkit.command.CommandSender;
 
+/**
+ * The command for reloading the plugin.
+ */
 public class Reload extends AbstractSubCommand {
 
+    /**
+     * Constructs the Reload command.
+     */
     public Reload() {
         super("reload", null, null, "Reload the plugin's config", Permission.ADMIN);
     }
 
+    /**
+     * Reloads the plugin, refreshing values on the server to match changes in the config.
+     *
+     * @param sender the command's sender
+     * @param args the arguments passed
+     * @return false
+     */
     @Override
     public boolean commandRoutine(CommandSender sender, String[] args) {
         Utils.setDebugReceiver(sender);
