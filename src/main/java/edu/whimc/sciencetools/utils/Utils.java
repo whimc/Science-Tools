7f6472e17782c1a0d8fea7ef7cdee1cefb3b56af
emicb <emicb55@gmail.com>
2021-07-20 14:59:57 -0500

    Added Comments To Utils.java

--- src/main/java/edu/whimc/sciencetools/utils/Utils.java
+++ src/main/java/edu/whimc/sciencetools/utils/Utils.java
@@ -6,10 +6,19 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 
+/**
+ * All of the utility functions.
+ */
 public class Utils {
 
     private static CommandSender debugReceiver = null;
 
+    /**
+     * Parses a String and returns a Double.
+     *
+     * @param str the String to parse
+     * @return the Double parsed from the String
+     */
     public static Double parseDouble(String str) {
         try {
             return Double.valueOf(str.replace(",", ""));
@@ -18,6 +27,13 @@ public class Utils {
         }
     }
 
+    /**
+     * Parses a String and returns a Double. Throws a chat error if String is not a valid number.
+     *
+     * @param sender the command's sender
+     * @param str the String to parse
+     * @return the Double parsed from the String
+     */
     public static Double parseDoubleWithError(CommandSender sender, String str) {
         Double res = parseDouble(str);
         if (res == null) {
@@ -27,10 +43,22 @@ public class Utils {
         return res;
     }
 
+    /**
+     * Checks if the WorldGuard plugin is enabled.
+     *
+     * @return whether WorldGuard is enabled
+     */
     public static boolean worldGuardEnabled() {
         return Bukkit.getPluginManager().isPluginEnabled("WorldGuard");
     }
 
+    /**
+     * Trims decimal to desired level of precision.
+     *
+     * @param val the value to be trimmed
+     * @param precision the desired level of precision
+     * @return a String decimal trimmed to the desired level of precision
+     */
     public static String trimDecimals(double val, int precision) {
         return String.format("%,." + precision + "f", val);
     }
@@ -43,6 +71,11 @@ public class Utils {
         debugReceiver = sender;
     }
 
+    /**
+     * Logs the message.
+     *
+     * @param message the message to log
+     */
     public static void log(String message) {
         // Make tabs more consistent
         message = message.replace("\t", "    ");
@@ -52,12 +85,25 @@ public class Utils {
         ScienceTools.getInstance().getLogger().info(colored(message));
     }
 
+    /**
+     * Sends a message to the command's sender.
+     *
+     * @param sender the command's sender
+     * @param strs the Strings to be sent
+     */
     public static void msg(CommandSender sender, String... strs) {
         for (String str : strs) {
             sender.sendMessage(colored(str));
         }
     }
 
+    /**
+     * Translates a string using '&' into a string that uses the internal ChatColor.COLOR_CODE color code character.
+     * The alternate color code character will only be replaced if it is immediately followed by 0-9, A-F, a-f, K-O, k-o, R or r.
+     *
+     * @param str the String to translate
+     * @return the translated String
+     */
     public static String colored(String str) {
         return ChatColor.translateAlternateColorCodes('&', str);
     }
