4bb7bff93165436c748ac6c74789a2b341d7b4e8
emicb <emicb55@gmail.com>
2021-07-19 20:08:15 -0500

    Added Comments To CommandError.java

--- src/main/java/edu/whimc/sciencetools/commands/CommandError.java
+++ src/main/java/edu/whimc/sciencetools/commands/CommandError.java
@@ -1,10 +1,19 @@
 package edu.whimc.sciencetools.commands;
 
+/**
+ * A command error runtime exception.
+ */
 public class CommandError extends RuntimeException {
 
     private final String message;
     private final boolean sendUsage;
 
+    /**
+     * Constructs the CommandError.
+     *
+     * @param message the message for the error to display in-game
+     * @param sendUsage whether or not to send the command usage
+     */
     public CommandError(String message, boolean sendUsage) {
         this.message = message;
         this.sendUsage = sendUsage;
