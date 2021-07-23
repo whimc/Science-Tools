6c8d6bd8cc94bef09e06892da234c496436f1051
emicb <emicb55@gmail.com>
2021-07-23 14:49:51 -0500

    Added Comments To ConversionManager.java

--- src/main/java/edu/whimc/sciencetools/models/conversion/ConversionManager.java
+++ src/main/java/edu/whimc/sciencetools/models/conversion/ConversionManager.java
@@ -10,19 +10,29 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Handles operations regarding Conversions (adding, removing, saving, loading).
+ */
 public class ConversionManager {
 
     private final Map<String, Conversion> conversions;
 
+    /**
+     * Constructs a new ConversionManager.
+     */
     public ConversionManager() {
         this.conversions = new HashMap<>();
         loadConversions();
     }
 
+    /**
+     * Loads all conversions from the config.
+     */
     private void loadConversions() {
         FileConfiguration config = ScienceTools.getInstance().getConfig();
         Utils.log("&eLoading Conversions from config");
 
+        // collect conversions from config
         for (String conversion : config.getConfigurationSection("conversions").getKeys(false)) {
             Utils.log("&b - &f" + conversion);
             String expr = config.getString("conversions." + conversion + ".expression");
@@ -31,6 +41,7 @@ public class ConversionManager {
             Utils.log("&b\t- Expression: \"&f" + expr + "&b\"");
             Utils.log("&b\t- Unit: \"&f" + unit + "&b\"");
 
+            // ensure conversion's expression is valid, skip if not
             JSNumericExpression jsExpr = new JSNumericExpression(expr);
             if (!jsExpr.valid()) {
                 Utils.log("&c\t* Invalid expression! Skipping.");
@@ -43,38 +54,79 @@ public class ConversionManager {
         Utils.log("&eConversions loaded!");
     }
 
+    /**
+     * Creates and loads a new Conversion.
+     *
+     * @param name the name of the Conversion
+     * @param unit the unit being converted to
+     * @param expr the Conversion equation
+     * @return the Conversion
+     */
     private @NotNull Conversion loadConversion(String name, String unit, JSNumericExpression expr) {
         Conversion conversion = new Conversion(name, unit, expr);
         conversions.put(name, conversion);
         return conversion;
     }
 
+    /**
+     * Creates and saves a new Conversion to the config.
+     *
+     * @param name the name of the Conversion
+     * @param unit the unit being converted to
+     * @param expr the Conversion equation
+     * @return the Conversion
+     */
     public Conversion createConversion(String name, String unit, JSNumericExpression expr) {
         Conversion conversion = loadConversion(name, unit, expr);
         saveToConfig(conversion);
         return conversion;
     }
 
+    /**
+     * Removes the specified Conversion.
+     *
+     * @param conversion the Conversion to remove
+     */
     public void removeConversion(@NotNull Conversion conversion) {
         String name = conversion.getName();
         conversions.remove(name);
         setConfig(name, null);
     }
 
+    /**
+     * Gets the specified Conversion.
+     *
+     * @param key the Conversion's name
+     * @return the specified Conversion
+     */
     public Conversion getConversion(String key) {
         return this.conversions.getOrDefault(key, null);
     }
 
+    /**
+     * @return the Collection of Conversions
+     */
     public Collection<Conversion> getConversions() {
         return conversions.values();
     }
 
+    /**
+     * Saves the Conversion to the config.
+     *
+     * @param conversion the conversion to save
+     */
     public void saveToConfig(@NotNull Conversion conversion) {
         String name = conversion.getName();
         setConfig(name + ".unit", conversion.getUnit());
         setConfig(name + ".expression", conversion.getExpression().toString());
     }
 
+    /**
+     * Sets and saves a Conversion to the config.
+     *
+     * @param key the Conversion's name
+     * @param value the Conversion
+     */
     private void setConfig(String key, Object value) {
         ScienceTools.getInstance().getConfig().set("conversions." + key, value);
         ScienceTools.getInstance().saveConfig();
