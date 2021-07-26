0913c8f8b7d0a297dc9edb28d2f053dbfcda814c
emicb <emicb55@gmail.com>
2021-07-26 11:42:39 -0500

    Added Comments To JSExpression.java

--- src/main/java/edu/whimc/sciencetools/javascript/JSExpression.java
+++ src/main/java/edu/whimc/sciencetools/javascript/JSExpression.java
@@ -1,9 +1,17 @@
 package edu.whimc.sciencetools.javascript;
 
+/**
+ * A JavaScript expression.
+ */
 public class JSExpression {
-
+    /* The JavaScript expression */
     private String expr;
 
+    /**
+     * Constructs a JSExpression.
+     *
+     * @param expr the JavaScript expression
+     */
     public JSExpression(String expr) {
         this.expr = expr;
     }
@@ -12,22 +20,48 @@ public class JSExpression {
         this.expr = newExpr;
     }
 
+    /**
+     * @return the JavaScript expression
+     */
     public String getExpression() {
         return this.expr;
     }
 
+    /**
+     * Checks if the JavaScript expression is valid.
+     *
+     * @return whether or not the JavaScript expression is valid
+     */
     public boolean valid() {
         return run(JSContext.create(), false) != null;
     }
 
+    /**
+     * Runs the JavaScript engine.
+     *
+     * @param ctx the JavaScript context
+     * @param throwCommandError whether or not to throw a command error
+     * @return the value returned from the execution of the script
+     */
     public Object run(JSContext ctx, boolean throwCommandError) {
         return JSEngine.run(getPreparedExpression(ctx), throwCommandError);
     }
 
+    /**
+     * Gets the JavaScript expression with placeholders swapped out for their replacements.
+     *
+     * @param ctx the JavaScript context
+     * @return the JavaScript expression with placeholders swapped out for their replacements
+     */
     protected String getPreparedExpression(JSContext ctx) {
         return JSPlaceholder.prepareExpression(ctx, this.expr);
     }
 
+    /**
+     * Converts the JSExpression into a String.
+     *
+     * @return the JSExpression as a String
+     */
     @Override
     public String toString() {
         return this.expr;
