ac895123b32e9b4f7307ac8da434dc782cea2eeb
emicb <emicb55@gmail.com>
2021-07-27 11:18:32 -0500

    Added Comments To ConversionsBase.java

--- src/main/java/edu/whimc/sciencetools/commands/subcommands/conversions/ConversionsBase.java
+++ src/main/java/edu/whimc/sciencetools/commands/subcommands/conversions/ConversionsBase.java
@@ -4,12 +4,25 @@ import edu.whimc.sciencetools.commands.subcommands.AbstractSubCommand;
 import edu.whimc.sciencetools.utils.Utils;
 import org.bukkit.command.CommandSender;
 
+/**
+ * The command to manage unit conversions. This is not yet implemented.
+ */
 public class ConversionsBase extends AbstractSubCommand {
 
+    /**
+     * Constructs a ConversionsBase.
+     */
     public ConversionsBase() {
         super("conversion", null, null, "Manage unit conversions", Permission.ADMIN);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @param sender the command's sender
+     * @param args the arguments passed
+     * @return false
+     */
     @Override
     public boolean commandRoutine(CommandSender sender, String[] args) {
         // TODO Implement this
