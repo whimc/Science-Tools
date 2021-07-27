56f582da4691c0509859a394b49850c10342a3d3
emicb <emicb55@gmail.com>
2021-07-27 11:18:42 -0500

    Added Comments To ToolsBase.java

--- src/main/java/edu/whimc/sciencetools/commands/subcommands/tools/ToolsBase.java
+++ src/main/java/edu/whimc/sciencetools/commands/subcommands/tools/ToolsBase.java
@@ -4,12 +4,25 @@ import edu.whimc.sciencetools.commands.subcommands.AbstractSubCommand;
 import edu.whimc.sciencetools.utils.Utils;
 import org.bukkit.command.CommandSender;
 
+/**
+ * The command to manage tools. This is not yet implemented.
+ */
 public class ToolsBase extends AbstractSubCommand {
 
+    /**
+     * Constructs a ToolsBase.
+     */
     public ToolsBase() {
         super("tool", null, null, "Manage the science tools", Permission.ADMIN);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @param sender the command's sender
+     * @param args the arguments passed
+     * @return false
+     */
     @Override
     public boolean commandRoutine(CommandSender sender, String[] args) {
         // TODO Implement this
