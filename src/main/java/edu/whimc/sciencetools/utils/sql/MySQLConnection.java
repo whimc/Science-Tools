c2fcfdf3aa2fc4780aba8b6a1b17e3535db85137
Jack Henhapl <jackah2@illinois.edu>
2022-01-13 19:20:09 -0600

    Using connection pool instead of single connection

--- src/main/java/edu/whimc/sciencetools/utils/sql/MySQLConnection.java
+++ src/main/java/edu/whimc/sciencetools/utils/sql/MySQLConnection.java
@@ -1,27 +1,21 @@
 package edu.whimc.sciencetools.utils.sql;
 
+import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import edu.whimc.sciencetools.ScienceTools;
 import edu.whimc.sciencetools.utils.sql.migration.SchemaManager;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import org.bukkit.configuration.ConfigurationSection;
 
 /**
  * Handles creating connections to the configured MySQL database.
  */
 public class MySQLConnection {
 
-    private static final String URL_TEMPLATE = "jdbc:mysql://%s:%s/%s";
-
-    private final String host;
-    private final String database;
-    private final String username;
-    private final String password;
-    private final String url;
-    private final int port;
     private final ScienceTools plugin;
 
-    private Connection connection;
+    private MysqlDataSource dataSource;
 
     /**
      * Create an instance of {@link MySQLConnection}.
@@ -29,15 +23,15 @@ public class MySQLConnection {
      * @param plugin The plugin instance.
      */
     public MySQLConnection(ScienceTools plugin) {
-        this.host = plugin.getConfig().getString("mysql.host", "localhost");
-        this.port = plugin.getConfig().getInt("mysql.port", 3306);
-        this.database = plugin.getConfig().getString("mysql.database", "minecraft");
-        this.username = plugin.getConfig().getString("mysql.username", "user");
-        this.password = plugin.getConfig().getString("mysql.password", "pass");
-
-        this.url = String.format(URL_TEMPLATE, this.host, this.port, this.database);
-
         this.plugin = plugin;
+        this.dataSource = new MysqlConnectionPoolDataSource();
+
+        ConfigurationSection config = plugin.getConfig();
+        this.dataSource.setServerName(config.getString("mysql.host", "localhost"));
+        this.dataSource.setPortNumber(config.getInt("mysql.port", 3306));
+        this.dataSource.setDatabaseName(config.getString("mysql.database", "minecraft"));
+        this.dataSource.setUser(config.getString("mysql.username", "user"));
+        this.dataSource.setPassword(config.getString("mysql.password", "pass"));
     }
 
     /**
@@ -46,29 +40,28 @@ public class MySQLConnection {
      * @return Whether the database was successfully initialized.
      */
     public boolean initialize() {
-        if (getConnection() == null) {
+        try (Connection connection = getConnection()) {
+            if (connection == null) {
+                return false;
+            }
+
+            SchemaManager manager = new SchemaManager(this.plugin, connection);
+            return manager.migrate();
+        } catch (SQLException unused) {
             return false;
         }
-
-        SchemaManager manager = new SchemaManager(this.plugin, this.connection);
-        return manager.migrate();
     }
 
     /**
      * A connection to the configured database or null if unsuccessful.
      */
-    public Connection getConnection() {
-        try {
-            if (this.connection != null && !this.connection.isClosed()) {
-                return this.connection;
-            }
-
-            this.connection = DriverManager.getConnection(this.url, this.username, this.password);
-        } catch (SQLException unused) {
+    public Connection getConnection() throws SQLException {
+        Connection connection = this.dataSource.getConnection();
+        if (!connection.isValid(1)) {
             return null;
         }
 
-        return this.connection;
+        return connection;
     }
 
 }
